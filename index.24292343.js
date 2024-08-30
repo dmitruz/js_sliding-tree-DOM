cc4ffa129f712f3ca8de3228938c1a1ae64b83bf
dmitruz <dmitruz970@gmail.com>
2024-08-30 22:02:41 +0300

    make build

---
+++ index.24292343.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll(".tree li").forEach(function(e){var t=document.createElement("span");t.textContent=e.firstChild.textContent.trim(),e.firstChild.replaceWith(t),t.addEventListener("click",function(t){var n=e.querySelector("ul");n&&(n.style.display="none"===n.style.display?"block":"none"),t.stopPropagation()})})});
+//# sourceMappingURL=index.24292343.js.map
